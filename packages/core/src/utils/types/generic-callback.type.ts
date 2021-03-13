b35a3c93cf47a5f2d42d59c9c6b6597a8aa45df0
Julien Pinto <Stnaire@users.noreply.github.com>
2021-03-13 14:26:26 +0100

    feat: GenericCallback now can define the arg and return types

--- packages/core/src/utils/types/generic-callback.type.ts
+++ packages/core/src/utils/types/generic-callback.type.ts
@@ -1,4 +1,4 @@
 /**
  * A generic function, taking any number of arguments and returning anything or nothing.
  */
-export type GenericCallback = (...args: any) => any;
+export type GenericCallback<T = any, R = any> = (...args: T[]) => R;
