b0dbde74d8253a25f5e7deaa9c7b706c5f60783e
Julien Pinto <Stnaire@users.noreply.github.com>
2021-03-26 08:59:20 +0100

    chore: add isPromiseLike tests

--- packages/utils/src/__tests__/utils-types.test.ts
+++ packages/utils/src/__tests__/utils-types.test.ts
@@ -1,8 +1,55 @@
 import 'reflect-metadata';
+import { noop } from "../noop";
+import { GenericCallback } from "../types/generic-callback.type";
+import { isPojo } from "../types/is-pojo";
 import { isPromiseLike } from "../types/is-promise-like";
 
-test('isPromiseLike', () => {
-    expect(isPromiseLike({ then: () => null })).toBe(true);
-    expect(isPromiseLike(null)).toBe(false);
-    expect(isPromiseLike({})).toBe(false);
+class Foo {}
+
+let currentTestingFunction: GenericCallback = noop;
+function createTest(description: string, expected: boolean, ...args: any[]) {
+    test(description, () => {
+        expect(currentTestingFunction.apply(null, args)).toBe(expected);
+    });
+}
+
+describe('isPromiseLike', () => {
+    beforeAll(() => {
+        currentTestingFunction = isPromiseLike;
+    });
+    createTest('{ then: () => null }', true, { then: () => null });
+    createTest('null', false, null);
+    createTest('{}', false, {});
+});
+
+describe('isPojo', () => {
+    beforeAll(() => {
+        currentTestingFunction = isPojo;
+    });
+    createTest('empty object',  true, {});
+    createTest('Object.create(null)',  true, Object.create(null));
+    createTest('function() {}', false, function() {});
+    createTest('[]', false, []);
+    createTest('new Date()', false, new Date());
+    createTest('true', false, true);
+    createTest('"abc"', false, "abc");
+    createTest('123', false, 123);
+    createTest('RegExp', false, new RegExp(/.*/));
+    createTest('null', false, null);
+    createTest('undefined', false, undefined);
+    createTest('Object.create({})', false, Object.create({}));
+    createTest('new Foo()', false, new Foo());
+    createTest('{constructor: Foo}', false, {constructor: Foo});
+    createTest('{constructor: "Foo"}', true, {constructor: "Foo"});
+    createTest('function as value',  false, {
+        num: 2,
+        str: 'string',
+        func: () =>  2
+    });
+    createTest('function as deep value',  false, {
+        sub: {func: () => 2}
+    });
+    createTest('function as deep value (shallow check)',  true, {
+        sub: {func: () => 2}
+    }, false);
 });
