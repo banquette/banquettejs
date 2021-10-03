535401de1180aa67e4f315eabd8f017a52fedd7d
Julien Pinto <Stnaire@users.noreply.github.com>
2021-10-03 17:20:13 +0200

    feat: `vue-dom-module` package added

---
+++ packages/vue-dom-module/src/vue.module.ts
@@ -0,0 +1,27 @@
+import { AbstractDomModule, DomModule } from "@banquette/dom-modules";
+import { VueBuilder } from "@banquette/vue-typescript";
+
+@DomModule('vue')
+class Vue extends AbstractDomModule {
+    /**
+     * @inheritDoc
+     */
+    public getDefaultOptionName(): string | null {
+        return 'group';
+    }
+
+    /**
+     * @inheritDoc
+     */
+    protected bind(): void {
+        VueBuilder.CreateAppAndMount(
+            this.element,
+            this.getOption('group', '*'),
+            {
+                compilerOptions: {
+                    delimiters: ['${', '}']
+                }
+            }
+        );
+    }
+}
