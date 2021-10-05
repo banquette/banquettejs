733260809e6a1ade20663d1fbb468267d0fb5130
Julien Pinto <Stnaire@users.noreply.github.com>
2021-10-05 14:23:37 +0200

    chore: "ObservablePromiseInterface" now inherits "Promise"

--- packages/promise/src/observable-promise.interface.ts
+++ packages/promise/src/observable-promise.interface.ts
@@ -1,12 +1,7 @@
 import { Constructor } from "@banquette/utils-type";
-import { onFinallyCallback, onRejectCallback, onResolveCallback } from "./types";
+import { onRejectCallback, onResolveCallback } from "./types";
 
-export interface ObservablePromiseInterface<CompleteT> {
-    /**
-     * For compatibility with Promise<T>.
-     */
-    readonly [Symbol.toStringTag]: string;
-
+export interface ObservablePromiseInterface<CompleteT> extends Promise<CompleteT> {
     /**
      * Attaches callbacks for the resolution, rejection and/or progress events of the promise.
      */
@@ -15,11 +10,6 @@ export interface ObservablePromiseInterface<CompleteT> {
         onReject?: onRejectCallback<RejectT> | null,
         onProgress?: (progress: any /** No generic type to keep the type compatible with the official promise signature */) => void,
         progressTypes?: Array<Constructor>): ObservablePromiseInterface<ResultT|RejectT>;
-
-    /**
-     * Attaches a callback that will be called if the promise rejects.
-     */
-    catch<RejectT = never>(onReject?: onRejectCallback<RejectT> | null): ObservablePromiseInterface<CompleteT|RejectT>;
 
     /**
      * Like catch() but only calling the callback if the rejection reason is an object matching of the the type defined in parameter.
@@ -35,11 +25,6 @@ export interface ObservablePromiseInterface<CompleteT> {
      * Subscribe to the promise progression events.
      */
     progress<ProgressT = any>(onProgress: (progress: ProgressT) => void, types?: Array<Constructor>): ObservablePromiseInterface<CompleteT>;
-
-    /**
-     * Attaches a callback that will be called when the promise is settled, no matter if it resolves or rejects.
-     */
-    finally<ResultT = CompleteT, RejectT = never>(onSettle?: onFinallyCallback<ResultT> | null): ObservablePromiseInterface<ResultT|RejectT>;
 
     /**
      * Forces the rejection of the promise with a CancelException.
