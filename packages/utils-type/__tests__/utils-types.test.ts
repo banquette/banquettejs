27326090c03e526f05ff73a4374601450a06a932
Julien Pinto <Stnaire@users.noreply.github.com>
2021-10-08 15:05:22 +0200

    test: refactor utils-types tests and add isObject and isPrimitive tests

--- packages/utils-type/__tests__/utils-types.test.ts
+++ packages/utils-type/__tests__/utils-types.test.ts
@@ -1,53 +1,175 @@
 import 'reflect-metadata';
+import { isUndefined } from "lodash";
 import { noop } from "../src/utils";
-import { GenericCallback, isPojo, isPromiseLike } from "../src";
+import { isPojo, isPrimitive, isPromiseLike, isObject } from "../src";
 
 class Foo {}
 
-let currentTestingFunction: GenericCallback = noop;
+let currentTestingFunction: Function = noop;
+
+const candidateValues: Record<string, any> = {
+    '{}'                    : {},
+    'Object.create(null)'   : Object.create(null),
+    'function() {}'         : function () {},
+    '[]'                    : [],
+    'new Date()'            : new Date(),
+    'true'                  : true,
+    '"abc"'                 : "abc",
+    '123'                   : 123,
+    'RegExp'                : new RegExp(/.*/),
+    'null'                  : null,
+    'undefined'             : undefined,
+    'Object.create({})'     : Object.create({}),
+    'new Foo()'             : new Foo(),
+    '{constructor: Foo}'    : {constructor: Foo},
+    '{constructor: "Foo"}'  : {constructor: "Foo"},
+    '{ then: () => null }'  : { then: () => null },
+    '{ then: 2 }'           : { then: 2 },
+    'new Promise(...)'      : new Promise(() => {}),
+    'function in obj'       : {num: 2, str: 'string', func: () => 2},
+    'function in obj (deep)': {sub: {func: () => 2}}
+};
+
 function createTest(description: string, expected: boolean, ...args: any[]) {
     test(description, () => {
         expect(currentTestingFunction.apply(null, args)).toBe(expected);
     });
 }
 
-describe('isPromiseLike', () => {
+function createTestSuite(testFunction: Function, tests: Record<string, boolean>) {
     beforeAll(() => {
-        currentTestingFunction = isPromiseLike;
+        currentTestingFunction = testFunction;
     });
-    createTest('{ then: () => null }', true, { then: () => null });
-    createTest('null', false, null);
-    createTest('{}', false, {});
+    for (const key of Object.keys(tests)) {
+        if (!isUndefined(candidateValues[key]) || key === 'undefined') {
+            createTest(key, tests[key], candidateValues[key]);
+        } else {
+            throw `Candidate value "${key}" not found. Maybe update your test?`;
+        }
+    }
+}
+
+describe('isPromiseLike', () => {
+    createTestSuite(isPromiseLike, {
+        '{}'                    : false,
+        'Object.create(null)'   : false,
+        'function() {}'         : false,
+        '[]'                    : false,
+        'new Date()'            : false,
+        'true'                  : false,
+        '"abc"'                 : false,
+        '123'                   : false,
+        'RegExp'                : false,
+        'null'                  : false,
+        'undefined'             : false,
+        'Object.create({})'     : false,
+        'new Foo()'             : false,
+        '{constructor: Foo}'    : false,
+        '{constructor: "Foo"}'  : false,
+        '{ then: () => null }'  : true,
+        '{ then: 2 }'           : false,
+        'new Promise(...)'      : true,
+        'function in obj'       : false,
+        'function in obj (deep)': false
+    });
 });
 
 describe('isPojo', () => {
-    beforeAll(() => {
-        currentTestingFunction = isPojo;
+    createTestSuite(isPojo, {
+        '{}'                    : true,
+        'Object.create(null)'   : true,
+        'function() {}'         : false,
+        '[]'                    : false,
+        'new Date()'            : false,
+        'true'                  : false,
+        '"abc"'                 : false,
+        '123'                   : false,
+        'RegExp'                : false,
+        'null'                  : false,
+        'undefined'             : false,
+        'Object.create({})'     : false,
+        'new Foo()'             : false,
+        '{constructor: Foo}'    : false,
+        '{constructor: "Foo"}'  : true,
+        '{ then: () => null }'  : false,
+        '{ then: 2 }'           : true,
+        'new Promise(...)'      : false,
+        'function in obj'       : false,
+        'function in obj (deep)': false
     });
-    createTest('empty object',  true, {});
-    createTest('Object.create(null)',  true, Object.create(null));
-    createTest('function() {}', false, function() {});
-    createTest('[]', false, []);
-    createTest('new Date()', false, new Date());
-    createTest('true', false, true);
-    createTest('"abc"', false, "abc");
-    createTest('123', false, 123);
-    createTest('RegExp', false, new RegExp(/.*/));
-    createTest('null', false, null);
-    createTest('undefined', false, undefined);
-    createTest('Object.create({})', false, Object.create({}));
-    createTest('new Foo()', false, new Foo());
-    createTest('{constructor: Foo}', false, {constructor: Foo});
-    createTest('{constructor: "Foo"}', true, {constructor: "Foo"});
-    createTest('function as value',  false, {
-        num: 2,
-        str: 'string',
-        func: () =>  2
+});
+
+describe('isPojo (deep: false)', () => {
+    createTestSuite((value: any) => isPojo(value, false), {
+        '{}'                    : true,
+        'Object.create(null)'   : true,
+        'function() {}'         : false,
+        '[]'                    : false,
+        'new Date()'            : false,
+        'true'                  : false,
+        '"abc"'                 : false,
+        '123'                   : false,
+        'RegExp'                : false,
+        'null'                  : false,
+        'undefined'             : false,
+        'Object.create({})'     : false,
+        'new Foo()'             : false,
+        '{constructor: Foo}'    : true,
+        '{constructor: "Foo"}'  : true,
+        '{ then: () => null }'  : true,
+        '{ then: 2 }'           : true,
+        'new Promise(...)'      : false,
+        'function in obj'       : true,
+        'function in obj (deep)': true
     });
-    createTest('function as deep value',  false, {
-        sub: {func: () => 2}
+});
+
+describe('isObject', () => {
+    createTestSuite(isObject, {
+        '{}'                    : true,
+        'Object.create(null)'   : true,
+        'function() {}'         : false,
+        '[]'                    : true,
+        'new Date()'            : true,
+        'true'                  : false,
+        '"abc"'                 : false,
+        '123'                   : false,
+        'RegExp'                : true,
+        'null'                  : false,
+        'undefined'             : false,
+        'Object.create({})'     : true,
+        'new Foo()'             : true,
+        '{constructor: Foo}'    : true,
+        '{constructor: "Foo"}'  : true,
+        '{ then: () => null }'  : true,
+        '{ then: 2 }'           : true,
+        'new Promise(...)'      : true,
+        'function in obj'       : true,
+        'function in obj (deep)': true
     });
-    createTest('function as deep value (shallow check)',  true, {
-        sub: {func: () => 2}
-    }, false);
+});
+
+describe('isPrimitive', () => {
+    createTestSuite(isPrimitive, {
+        '{}'                    : false,
+        'Object.create(null)'   : false,
+        'function() {}'         : false,
+        '[]'                    : false,
+        'new Date()'            : false,
+        'true'                  : true,
+        '"abc"'                 : true,
+        '123'                   : true,
+        'RegExp'                : false,
+        'null'                  : true,
+        'undefined'             : true,
+        'Object.create({})'     : false,
+        'new Foo()'             : false,
+        '{constructor: Foo}'    : false,
+        '{constructor: "Foo"}'  : false,
+        '{ then: () => null }'  : false,
+        '{ then: 2 }'           : false,
+        'new Promise(...)'      : false,
+        'function in obj'       : false,
+        'function in obj (deep)': false
+    });
 });
