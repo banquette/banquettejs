3a2452136966195f92470152cd225c66dfbac0e3
Julien Pinto <Stnaire@users.noreply.github.com>
2021-10-09 23:55:53 +0200

    feat: new utility to convert enums to arrays

---
+++ packages/utils-array/src/enum-to-array.ts
@@ -0,0 +1,12 @@
+import { getObjectKeys } from "@banquette/utils-object";
+import { isNumeric } from "@banquette/utils-type";
+
+/**
+ * Convert an enum into a array containing its values.
+ */
+export function enumToArray<T>(enumeration: T): Array<T[keyof T]> {
+    return getObjectKeys(enumeration)
+        .filter(key => !isNumeric(key))
+        .map((key) => enumeration[key])
+        .filter(val => typeof val === "number" || typeof val === "string");
+}
--- packages/utils-array/src/index.ts
+++ packages/utils-array/src/index.ts
@@ -1,4 +1,5 @@
 
+export * from './enum-to-array';
 export * from './get-first-of';
 export * from './remove-from-array';
 export * from './trim-array';
