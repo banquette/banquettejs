539a30493f6fe994e63805fc9e3ac1657cd54ca1
Julien Pinto <Stnaire@users.noreply.github.com>
2021-11-03 10:47:42 +0100

    chore: add exports

--- packages/api/src/index.ts
+++ packages/api/src/index.ts
@@ -1,2 +1,17 @@
 
+export * from './api-configuration.interface';
+export * from './api-endpoint.options';
+export * from './api-endpoint';
+export * from './api-endpoint-collection';
+export * from './api-endpoint-parameter.interface';
+export * from './api-endpoints-configuration.interface';
+export * from './api-metadata.service';
+export * from './config';
 
+/**
+ * Exceptions
+ */
+export * from './exception/endpoint-not-found.exception';
+export * from './exception/invalid-parameter.exception';
+export * from './exception/missing-required-parameter.exception';
+export * from './exception/unsupported-parameters.exception';
