c472ce4ad5e57b1f928029b6558af4366225b6f0
Julien Pinto <Stnaire@users.noreply.github.com>
2021-11-09 21:50:06 +0100

    chore: remove the method check because it's impossible to be certain the property is not a method without creating an instance

--- packages/vue-typescript/src/decorator/watch.decorator.ts
+++ packages/vue-typescript/src/decorator/watch.decorator.ts
@@ -1,6 +1,3 @@
-import { UsageException } from "@banquette/exception";
-import { isNonEmptyString } from "@banquette/utils-string";
-import { isFunction } from "@banquette/utils-type";
 import { WatchOptions } from "@vue/runtime-core";
 import { getDecoratorsData } from "../utils";
 import { DecoratorsDataInterface } from "./decorators-data.interface";
@@ -19,9 +16,6 @@ export interface WatchDecoratorOptions {
  */
 export function Watch(source: WatchSource, options: WatchOptions = {}): Function {
     return (prototype: any, propertyKey: string) => {
-        if (!isNonEmptyString(propertyKey) || !isFunction(prototype.constructor.prototype[propertyKey])) {
-            throw new UsageException('You can only use @Watch() on methods.');
-        }
         const data: DecoratorsDataInterface = getDecoratorsData(prototype);
         data.watch.push({
             target: propertyKey,
