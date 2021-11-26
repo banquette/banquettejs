eeee485704d5b9ef34a987b53d9597a9a6dc2c86
Julien Pinto <Stnaire@users.noreply.github.com>
2021-11-26 10:53:30 +0100

    chore: add missing id

--- packages/exception/src/type/not-implemented.exception.ts
+++ packages/exception/src/type/not-implemented.exception.ts
@@ -4,6 +4,8 @@ import { Exception } from "../exception";
  * Exception thrown to clearly notify an implementation is missing.
  */
 export class NotImplementedException extends Exception {
+    public readonly id: string = 'not-implemented';
+
     public constructor(message: string = 'Not implemented.') {
         super(message);
     }
