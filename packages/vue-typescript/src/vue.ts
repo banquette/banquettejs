edbf10565318da568cab0c6f701af8927c98fb32
Julien Pinto <Stnaire@users.noreply.github.com>
2021-11-29 01:32:22 +0100

    feat: add a `getParent` method to the `Vue` base class to search for a specific type of component in the parents hierarchy

--- packages/vue-typescript/src/vue.ts
+++ packages/vue-typescript/src/vue.ts
@@ -2,9 +2,11 @@ import { isArray } from "@banquette/utils-type/is-array";
 import { isObject } from "@banquette/utils-type/is-object";
 import { isString } from "@banquette/utils-type/is-string";
 import { isType } from "@banquette/utils-type/is-type";
+import { Constructor } from "@banquette/utils-type/types";
 import { WatchOptions } from "@vue/runtime-core";
 import { ComponentPublicInstance, ComponentInternalInstance, Slots, WatchStopHandle, VNode } from "vue";
 import { DECORATORS_CTOR_NAME } from "./constants";
+import { isComponentInstance } from "./utils";
 
 /**
  * Fake implementation of the public attributes of the vue instance.
@@ -34,8 +36,22 @@ export class Vue implements ComponentPublicInstance {
     /**
      * Test if a slot is defined and not empty.
      */
-    hasSlot(name: string): boolean {
+    public hasSlot(name: string): boolean {
         return isObject(this.$slots) && Object.keys(this.$slots).indexOf(name) > -1;
+    }
+
+    /**
+     * Try to get a reference on a specific parent component.
+     */
+    protected getParent<T extends Constructor<Vue>>(component: T): InstanceType<T>|null {
+        let $parent = this.$parent;
+        while ($parent !== null) {
+            if (isComponentInstance($parent, component)) {
+                return $parent;
+            }
+            $parent = $parent.$parent;
+        }
+        return null;
     }
 
     /**
