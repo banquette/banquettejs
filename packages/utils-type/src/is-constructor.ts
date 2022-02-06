23c1552392c3f9edcf92583c5f3c59a37ef20268
Julien Pinto <Stnaire@users.noreply.github.com>
2022-02-06 23:01:32 +0100

    chore: faster implementation of `isConstructor` that doesn't rely on exceptions

--- packages/utils-type/src/is-constructor.ts
+++ packages/utils-type/src/is-constructor.ts
@@ -4,10 +4,5 @@ import { Constructor } from "./types";
  * Test if the input value is an object constructor.
  */
 export function isConstructor(value: any): value is Constructor {
-    try {
-        new new Proxy(value, {construct() { return {}; }});
-        return true;
-    } catch (err) {
-        return false;
-    }
+    return typeof(value) === 'function' && !!value.prototype && value.prototype.constructor === value;
 }
