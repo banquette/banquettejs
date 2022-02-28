c7c2f55c9996105b1bbe8957df348721a880ad9d
Julien Pinto <Stnaire@users.noreply.github.com>
2022-03-01 00:51:04 +0100

    chore: add credit to `gullerya/object-observer`

--- packages/object-observer/src/index.ts
+++ packages/object-observer/src/index.ts
@@ -1,3 +1,17 @@
+/**
+ * This package is inspired by `gullerya/object-observer`.
+ *
+ * Very few code is directly reused but the main spirit of the implementation is the same.
+ * Unit tests are also inspired by it.
+ *
+ * @see https://github.com/gullerya/object-observer
+ *
+ * The main difference, expect for the internal implementation, is that `gullerya/object-observer` doesn't mutate
+ * the observed object, but make a clone as a literal object.
+ *
+ * The @banquette version DOES mutate the objects being observed so its possible to observe complex objects (like classes)
+ * while preserving their logic, inheritance, etc.
+ */
 import { ArrayObserver } from "./observer/array.observer";
 import { ObjectObserver } from "./observer/object.observer";
 import { ObserverFactory } from "./observer.factory";
