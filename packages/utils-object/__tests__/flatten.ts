b633970c28cea112e5d7218dd0e18f032b46b810
Julien Pinto <Stnaire@users.noreply.github.com>
2022-03-04 10:23:14 +0100

    test: add tests for the `flatten` utility

---
+++ packages/utils-object/__tests__/flatten.ts
@@ -0,0 +1,107 @@
+import { createGenericTestSuite } from "../../__tests__/utils";
+import { flatten } from "../src";
+
+const BASE_OBJ = {
+    foo: {
+        bar: {
+            a: 'a',
+            b: {
+                b1: 'b1',
+                b2: 'b2',
+                b3: 'b3',
+            },
+            c: 'c'
+        },
+        baz: 'baz'
+    },
+    second: 'second'
+};
+
+describe('flatten', () => {
+    createGenericTestSuite(flatten, [
+        [BASE_OBJ, {
+            'foo.bar.a': 'a',
+            'foo.bar.b.b1': 'b1',
+            'foo.bar.b.b2': 'b2',
+            'foo.bar.b.b3': 'b3',
+            'foo.bar.c': 'c',
+            'foo.baz': 'baz',
+            'second': 'second'
+        }]
+    ], 'toStrictEqual');
+
+    createGenericTestSuite(flatten, [
+        [BASE_OBJ, [{
+            'foo->bar->a': 'a',
+            'foo->bar->b->b1': 'b1',
+            'foo->bar->b->b2': 'b2',
+            'foo->bar->b->b3': 'b3',
+            'foo->bar->c': 'c',
+            'foo->baz': 'baz',
+            'second': 'second'
+        }, '->']]
+    ], 'toStrictEqual');
+
+    createGenericTestSuite(flatten, [
+        [BASE_OBJ, [{
+            'foo_bar': {
+                a: 'a',
+                b: {
+                    b1: 'b1',
+                    b2: 'b2',
+                    b3: 'b3'
+                },
+                c: 'c'
+            },
+            'foo_baz': 'baz',
+            'second': 'second'
+        }, '_', 1]]
+    ], 'toStrictEqual');
+
+    createGenericTestSuite(flatten, [
+        [BASE_OBJ, [{
+            'foo.bar.a': 'a',
+            'foo.bar.b': {
+                b1: 'b1',
+                b2: 'b2',
+                b3: 'b3'
+            },
+            'foo.bar.c': 'c',
+            'foo.baz': 'baz',
+            'second': 'second'
+        }, '.', 2]]
+    ], 'toStrictEqual');
+
+    createGenericTestSuite(flatten, [
+        [BASE_OBJ, [{
+            'foo.bar': {a: 'a', c: 'c'},
+            'foo.bar.b': {
+                b1: 'b1',
+                b2: 'b2',
+                b3: 'b3'
+            },
+            'foo': {baz: 'baz'},
+            'second': 'second'
+        }, '.', -1]]
+    ], 'toStrictEqual');
+
+    createGenericTestSuite(flatten, [
+        [BASE_OBJ, [{
+            foo: {
+                bar: {
+                    a: 'a',
+                    c: 'c'
+                },
+                baz: 'baz'
+            },
+            'foo.bar': {
+                b: {
+                    b1: 'b1',
+                    b2: 'b2',
+                    b3: 'b3'
+                }
+            },
+            second: 'second'
+        }, '.', -2]]
+    ], 'toStrictEqual');
+});
