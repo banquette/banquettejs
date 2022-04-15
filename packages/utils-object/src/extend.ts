e86cdf093ce4d8a2e284ffdc42b2b3263364f585
Julien Pinto <Stnaire@users.noreply.github.com>
2022-04-15 09:25:13 +0200

    chore: add cloning support for getters/setters

--- packages/utils-object/src/extend.ts
+++ packages/utils-object/src/extend.ts
@@ -7,12 +7,13 @@ import { isFunction } from "@banquette/utils-type/is-function";
 import { isObject } from "@banquette/utils-type/is-object";
 import { isPromiseLike } from "@banquette/utils-type/is-promise-like";
 import { isRegExp } from "@banquette/utils-type/is-reg-exp";
+import { isUndefined } from "@banquette/utils-type/is-undefined";
 
 /**
  * Copy values from objs into dst, optionally recursively.
  * Promises are copied as is, so they will be shared between dst and objs.
  */
-export function extend(dst: any, objs: any, deep: boolean = true) {
+export function extend(dst: any, objs: any, deep: boolean = true): any {
     objs = ensureArray(objs);
     for (let i = 0, ii = objs.length; i < ii; ++i) {
         const obj = objs[i];
@@ -22,8 +23,12 @@ export function extend(dst: any, objs: any, deep: boolean = true) {
         const keys = Object.keys(obj);
         for (let j = 0, jj = keys.length; j < jj; j++) {
             const key = keys[j];
+            const descriptor = Object.getOwnPropertyDescriptor(obj, key);
+            if (!isUndefined(descriptor) && (!isUndefined(descriptor.get) || !isUndefined(descriptor.set))) {
+                Object.defineProperty(dst, key, descriptor);
+                continue ;
+            }
             const src = obj[key];
-
             if (deep && isObject(src)) {
                 if (isDate(src)) {
                     dst[key] = new Date(src.valueOf());
