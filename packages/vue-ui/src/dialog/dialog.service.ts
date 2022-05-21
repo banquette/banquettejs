d5c86d58501ed7292fba69c6e103ccc038ce30e2
Julien Pinto <Stnaire@users.noreply.github.com>
2022-05-21 23:20:34 +0200

    chore: more the global functions definition

--- packages/vue-ui/src/dialog/dialog.service.ts
+++ packages/vue-ui/src/dialog/dialog.service.ts
@@ -1,6 +1,8 @@
 import { Inject } from "@banquette/dependency-injection/decorator/inject.decorator";
 import { Service } from "@banquette/dependency-injection/decorator/service.decorator";
+import { Injector } from "@banquette/dependency-injection/injector";
 import { EventDispatcherService } from "@banquette/event/event-dispatcher.service";
+import { VueBuilder } from "@banquette/vue-typescript/vue-builder";
 import { DialogEvents } from "./constant";
 import { HideDialogEventArg } from "./event/hide-dialog.event-arg";
 import { ShowDialogEventArg } from "./event/show-dialog.event-arg";
@@ -32,3 +34,18 @@ export class DialogService {
         this.eventDispatcher.dispatch(DialogEvents.HideAll);
     }
 }
+
+/**
+ * Register global utilities.
+ */
+VueBuilder.RegisterGlobalProperty('btShowDialog', (id: string, args: Record<string, any> = {}) => {
+    Injector.Get(DialogService).show(id, args);
+});
+
+VueBuilder.RegisterGlobalProperty('btHideDialog', (id: string) => {
+    Injector.Get(DialogService).hide(id);
+});
+
+VueBuilder.RegisterGlobalProperty('btHideAllDialogs', () => {
+    Injector.Get(DialogService).hideAll();
+});
--- packages/vue-ui/src/dialog/index.ts
+++ packages/vue-ui/src/dialog/index.ts
@@ -1,8 +1,4 @@
-import { VueBuilder } from "@banquette/vue-typescript/vue-builder";
-import { showDialog, hideDialog, hideAllDialogs } from "./utils";
 
 export { default as DialogComponent } from './dialog.component.vue';
-
-VueBuilder.RegisterGlobalProperty('btShowDialog', showDialog);
-VueBuilder.RegisterGlobalProperty('btHideDialog', hideDialog);
-VueBuilder.RegisterGlobalProperty('btHideAllDialogs', hideAllDialogs);
+export * from './dialog.service';
+export * from './constant';
