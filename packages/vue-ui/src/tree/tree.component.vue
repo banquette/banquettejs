b46fe5b572e53becc1ae537a470fcdaa616f88b0
Julien Pinto <Stnaire@users.noreply.github.com>
2022-05-26 17:23:46 +0200

    chore: fix invalid component name

--- packages/vue-ui/src/tree/tree.component.vue
+++ packages/vue-ui/src/tree/tree.component.vue
@@ -206,7 +206,7 @@ export default class TreeComponent extends Vue {
                 if (node.remotePending) {
                     leftAddon = h(resolveComponent("bt-progress-circular"));
                 } else if (node.fetched) {
-                    leftAddon = h(resolveComponent('icon-arrow-drop-down'));
+                    leftAddon = h(resolveComponent('i-material-arrow-drop-down'), {crop: true});
                 } else {
                     leftAddon = h('div', {class: 'unknown-text'}, '?');
                 }
