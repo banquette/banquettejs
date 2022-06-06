658288e8a10b48fbbf02f9ad38945d370698d56f
Julien Pinto <Stnaire@users.noreply.github.com>
2022-06-06 16:24:55 +0200

    chore: comment debug overlay

--- packages/vue-ui/src/debug/index.ts
+++ packages/vue-ui/src/debug/index.ts
@@ -1,2 +1,2 @@
-export * from './vue-debug-overlay';
+// export * from './vue-debug-overlay';
 export { default as FormControlStateOverlayComponent } from './form-control-state-overlay/form-control-state-overlay.component.vue';
