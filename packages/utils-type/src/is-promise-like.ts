716c37ef8e86a9d173e54a757722c4d2793a3622
Julien Pinto <Stnaire@users.noreply.github.com>
2022-08-10 21:47:18 +0200

    chore: better typing

--- packages/utils-type/src/is-promise-like.ts
+++ packages/utils-type/src/is-promise-like.ts
@@ -1,7 +1,7 @@
 /**
  * Test if the input looks like a promise.
  */
-export function isPromiseLike(value: any): boolean {
+export function isPromiseLike(value: any): value is PromiseLike<any> {
     // Avoid testing for "then()" to avoid invoking a proxy that could have side effects.
     return Object.prototype.toString.call(value).indexOf('Promise') > -1;
 }
