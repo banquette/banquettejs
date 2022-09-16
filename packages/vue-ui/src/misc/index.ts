1f5d0bfa4847106913f93a21ac2a06b68f9dc3f0
Stnaire <Stnaire@users.noreply.github.com>
2022-09-17 01:18:45 +0200

    chore: wrap the function calls so the tree shaking can remove them if unused

--- packages/vue-ui/src/misc/index.ts
+++ packages/vue-ui/src/misc/index.ts
@@ -7,5 +7,6 @@ export * from './click-outside.directive';
 export * from './collapsable.directive';
 export * from './stick-to.directive';
 export * from './teleport.directive';
+export * from './use-api-globals';
 
 export { default as ClientOnlyComponent } from './client-only.component.vue';
---
+++ packages/vue-ui/src/misc/use-api-globals.ts
@@ -0,0 +1,19 @@
+import { ApiService } from "@banquette/api/api.service";
+import { Injector } from "@banquette/dependency-injection/injector";
+import { proxy } from "@banquette/utils-misc/proxy";
+import { VueBuilder } from "@banquette/vue-typescript/vue-builder";
+
+export function useApiGlobals(): void {
+    const api = Injector.Get(ApiService);
+
+    /**
+     * Global Vue functions for making Api requests.
+     */
+    VueBuilder.RegisterGlobalProperty('btApiBuild', proxy(api.build, api));
+    VueBuilder.RegisterGlobalProperty('btApiSend', proxy(api.send, api));
+    VueBuilder.RegisterGlobalProperty('btApiGet', proxy(api.get, api));
+    VueBuilder.RegisterGlobalProperty('btApiPost', proxy(api.post, api));
+    VueBuilder.RegisterGlobalProperty('btApiPut', proxy(api.put, api));
+    VueBuilder.RegisterGlobalProperty('btApiPatch', proxy(api.patch, api));
+    VueBuilder.RegisterGlobalProperty('btApiDelete', proxy(api.delete, api));
+}
