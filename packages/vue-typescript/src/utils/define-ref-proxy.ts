62f7f902b80293b63640029100d8a3d9f734ac54
Stnaire <Stnaire@users.noreply.github.com>
2022-09-17 01:02:10 +0200

    feat: automatically reassign to the Vue's proxy any ref with a reassignable proxy as value

--- packages/vue-typescript/src/utils/define-ref-proxy.ts
+++ packages/vue-typescript/src/utils/define-ref-proxy.ts
@@ -1,3 +1,5 @@
+import { reassign } from "@banquette/utils-misc/make-reassignable";
+
 /**
  * Define a proxy making the wrapper object created by Vue invisible.
  */
@@ -5,9 +7,13 @@ export function defineRefProxy(proxy: any, proxyKey: string, target: any, target
     Object.defineProperty(proxy, proxyKey, {
         get: () => target[targetKey].value,
         set: (value) => {
-            target[targetKey].value = value
+            target[targetKey].value = value;
+
+            // If the value has been wrap into a proxy created by `makeReassignable()`,
+            // reassign it to the Vue proxy.
+            reassign(value, target[targetKey].value);
         },
         enumerable: true,
-        configurable: true,
+        configurable: true
     });
 }
