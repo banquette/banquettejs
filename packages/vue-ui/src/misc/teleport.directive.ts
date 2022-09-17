28e66c035368aa98c19c1d1745f01d4acb2328e8
Stnaire <Stnaire@users.noreply.github.com>
2022-09-18 00:22:24 +0200

    chore: `v-bt-teleport` now restores the teleported element to its original position when unmounted

--- packages/vue-ui/src/misc/teleport.directive.ts
+++ packages/vue-ui/src/misc/teleport.directive.ts
@@ -20,11 +20,25 @@ interface OptionsInterface {
     target?: object|Array<object|undefined>;
 }
 
+interface TeleportedItemInterface {
+    /**
+     * The element being teleported.
+     */
+    el: HTMLElement;
+
+    /**
+     * A comment placed where the element originally was, so we can restore it.
+     */
+    comment: Comment;
+}
+
 /**
  * A simple directive that will teleport a ref of a component (or array of components) into the host.
  */
 @Directive('bt-teleport')
 export class TeleportDirective {
+    private teleported: TeleportedItemInterface[] = [];
+
     public created(el: HTMLElement, bindings: DirectiveBinding): void {
         this.doTeleport(el, bindings);
     }
@@ -33,21 +47,46 @@ export class TeleportDirective {
         this.doTeleport(el, bindings);
     }
 
+    public unmounted(): void {
+        for (const item of this.teleported) {
+            item.comment.replaceWith(item.el);
+        }
+        this.teleported = [];
+    }
+
     private doTeleport(el: HTMLElement, bindings: DirectiveBinding) {
         const options = this.resolveOptions(bindings);
         const components = ensureArray(options.target);
+        const teleported: TeleportedItemInterface[] = [];
 
         for (const component of components) {
             if (!isUndefined(component)) {
-                if (options.ref !== null && component.$refs[options.ref] instanceof HTMLElement) {
-                    if (!el.contains(component.$refs[options.ref])) {
-                        el.appendChild(component.$refs[options.ref]);
+                const componentEl = options.ref !== null ? component.$refs[options.ref] : null;
+                if (componentEl instanceof HTMLElement) {
+                    let idx = this.teleported.findIndex((i) => i.el === componentEl);
+                    let commentEl: Comment;
+                    if (idx > -1) {
+                        commentEl = this.teleported[idx].comment;
+                    } else {
+                        commentEl = document.createComment('bt-teleport');
+                        componentEl.replaceWith(commentEl);
+                        el.appendChild(componentEl);
                     }
+                    teleported.push({
+                        el: componentEl,
+                        comment: commentEl
+                    });
                 } else {
                     console.warn(`Ref "${options.ref}" not found.`);
                 }
             }
         }
+        for (const previousTeleported of this.teleported) {
+            if (teleported.findIndex((i) => i.el === previousTeleported.el) < 0) {
+                previousTeleported.comment.replaceWith(previousTeleported.el);
+            }
+        }
+        this.teleported = teleported;
     }
 
     /**
