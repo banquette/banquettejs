5adf0a1539bcf8af6c6f9d0d63abc7229fa20880
Stnaire <Stnaire@users.noreply.github.com>
2022-09-26 14:16:44 +0200

    chore: remove unused code

--- packages/vue-ui/src/form/base-input/choices-module-user.interface.ts
+++
@@ -1,8 +0,0 @@
-import { ChoicesModule } from "@banquette/ui/form/select/choices/choices.module";
-
-/**
- * Ensure the object uses the `ChoicesModule`.
- */
-export interface ChoicesModuleUserInterface {
-    choices: ChoicesModule;
-}
