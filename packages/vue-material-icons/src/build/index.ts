222f7582871eafd587cde87e5e49a3aff1dcbcf1
Stnaire <Stnaire@users.noreply.github.com>
2022-10-10 08:41:25 +0200

    fix: add previously ignored icons

---
+++ packages/vue-material-icons/src/build/index.ts
@@ -0,0 +1,8 @@
+
+import { VueBuilder } from "@banquette/vue-typescript/vue-builder";
+import { default as IconMaterialBuild } from './build.component.vue';
+
+VueBuilder.RegisterComponent('i-material-build', IconMaterialBuild);
+
+export { IconMaterialBuild }
+
