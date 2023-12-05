a62e53462c8a43081603290510374cf5c598aea7
Stnaire <Stnaire@users.noreply.github.com>
2023-12-05 14:03:33 +0100

    fix: fix phone regex to enforce the validity and the beginning and end of the string

--- packages/validation/src/type/phone.ts
+++ packages/validation/src/type/phone.ts
@@ -11,6 +11,6 @@ export function Phone(options: ValidatorOptionsInterface|string = {}): Validator
      * Thanks to Dan for the regex.
      * @source https://www.regextester.com/1978
      */
-    const reg = /((?:\+|00)[17](?: |\-)?|(?:\+|00)[1-9]\d{0,2}(?: |\-)?|(?:\+|00)1\-\d{3}(?: |\-)?)?(0\d|\([0-9]{3}\)|[1-9]{0,3})(?:((?: |\-)[0-9]{2}){4}|((?:[0-9]{2}){4})|((?: |\-)[0-9]{3}(?: |\-)[0-9]{4})|([0-9]{7}))/;
+    const reg = /^((?:\+|00)[17](?: |\-)?|(?:\+|00)[1-9]\d{0,2}(?: |\-)?|(?:\+|00)1\-\d{3}(?: |\-)?)?(0\d|\([0-9]{3}\)|[1-9]{0,3})(?:((?: |\-)[0-9]{2}){4}|((?:[0-9]{2}){4})|((?: |\-)[0-9]{3}(?: |\-)[0-9]{4})|([0-9]{7}))$/;
     return Pattern(reg, assignOptionsDefaults(options, 'Must be a valid phone number.', 'phone'));
 }
